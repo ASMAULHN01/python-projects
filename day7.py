09e2016ab9de6fc59a9e5e72c539115f5a6cb36d
Asmaul Hoque <asmaulhn01@gmail.com>
2026-05-20 10:36:02 +0530

    Add files via upload

--- day7.py
+++ day7.py
@@ -1,19 +1,19 @@
 score = 0
 #Q1 
-ans = input("Q1. HTTP er Full Form ki").lower()
+ans = input("Q1. What is Full Form of HTTP? ").lower()
 if ans == "hypertext transfer protocall":
-    print("CORRECT, DARUN")
+    print("CORRECT, Good")
     score += 1
 else :
-    print(f"Worng! Answer : {ans}")
+    print("Worng! Answer : hypertext transfer protocall")
 #Q2
 
-ans = input ("Q2. HTTPS e S MANE KI BOJHAI ?").lower()
-if ans == "s mane secure":
-    print("CORRECT ! , Chaliye Ja")
+ans = input ("Q2. WHAT S means at HTTPS ?").lower()
+if ans == "s for secure":
+    print("CORRECT ! , Keep it up")
     score += 1
 else:
-    print(f"Wrong ! Answer {ans}")
+    print("Wrong ! Answer : s for secure")
 
 #Q3.
 ans = input ("Q3. Full Form of IP Adress ?").lower()
@@ -21,25 +21,29 @@ if ans == "internet protocall adress":
     print("CORRECT ! , Chaliye Ja")
     score += 1
 else :
-    print(f"Wrong ! Answer {ans}")
+    print("Wrong ! Answer :internet protocall adress ")
 
 #Q4. 
-ans = input ("Q3. OSI Model e Kota Layer Ache?").lower()
+ans = input ("Q4. How many layes's at OSI Model?").lower()
 if ans == "7":
     print("CORRECT ! , Excelent")
     score += 1
+else :
+    print("Wrong ! Answer : 7")
+
 #Q5.
-ans = input ("Q5. Firewall ki kaje lage ?").lower()
-if ans == "network protect kore" or "network security dey" or "protect kore" or "security" or " protect":
+ans = input ("Q5. what is the function of Firewall ?").lower()
+if ans == "network protecting " or "network security" or "protecting internet" or "security" or " protect":
     print("CORRECT ! , Almost Champion")
     score += 1
 else: 
-    print(f"Wrong ! Answer {ans}")
+    print("Wrong ! Answer: network protecting or network security or protecting internet or security or  protect")
+
 
 print(f"\n--- RESULT---")    
-print(f"\n Tomar Score: {score}/5")
+print(f"\n Your Score: {score}/5")
 if score == 5:
-    print("Outstanding ! Tui champion! 🏆")
+    print("Outstanding ! You are a champion! 🏆")
 elif score >= 3:
     print("Nice Try :Keep it up 🏆")
 else:
