32b41aad5931b186fb6283ed2a84ffc94c663f94
Asmaul Hoque <asmaulhn01@gmail.com>
2026-05-26 10:10:53 +0530

    Add files via upload

--- filehandeling.py
+++ filehandeling.py
@@ -12,6 +12,28 @@ log_login("Jubair", "Sucess")
 log_login("Fahad" , "Sucess")
 log_login("Unknown" , "Failed")
 
+chance = 3
+while chance > 0:
+    username = input("Enter your username : ")
+    password = input("Enter your password : ")
+
+    if username == "Asmaul" and password == "password123":
+        print("Login Sucessful !")
+        log_login(username, "Sucess")
+        break
+    elif username == "Jubair" and password == "jubair456":
+        print("Login sucessful !")
+        log_login(username, "Sucess")
+        break
+    else:
+        log_login(username, "Failed")
+        chance -= 1
+        if chance == 0:
+            print("⚠️ ALERT: Suspicious activity detected! Account locked.")
+        else:
+            print(f"Login Failed! Try Again. {chance} chances left.")
+
+
 print("Log file succesfully updated !")
 
 #log reading 
